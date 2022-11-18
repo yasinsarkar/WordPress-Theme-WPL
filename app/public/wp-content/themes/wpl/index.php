096c0ab871867b592850cc81857e6e13d321e93d
yasinsarkar <yasinsarkar68@gmail.com>
2022-11-18 23:21:45 +0600

    first commit

---
+++ app/public/wp-content/themes/wpl/index.php
@@ -0,0 +1,19 @@
+
+<?php
+/*
+*This Template for displaying the header
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes() ?>"class="no-js">
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
